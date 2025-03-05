14106a2ab18a31eff8520c232ef64c15d9aeb998
Mohamed-Selmi <mohamedamineselmi@outlook.com>
2025-03-05 21:37:23 +0100

    Add interface elements

---
+++ Interface.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+class PageOne:
+    def __init__(self):
+        self.root=Tk()
+        self.root.title("Gestion de bases des etudiants")
+        self.root.geometry('1280x720')
+        self.root.mainloop()
--- Main.py
+++ Main.py
@@ -1,17 +1,58 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from sql_connection import *
 from test import *
 from sqlcommands import *
 
-config=get_config()
-connection = start_connection(config)
-hello=("mohamed","amine","HELLO@gmail.com",15,8,2022)
+#config=get_config()
+#connection = start_connection(config)
+root = tk.Tk()
+root.title("Gestion de bases des etudiants")
+root.geometry('1280x720')
+root.minsize(1280,720)
+root.maxsize(1280,720)
+root.configure(background='#FFEAAE')
+frame1=tk.Frame(root, width=250, height=550,bg='#AFAAB9',borderwidth=2, relief="solid")
+frame1.place(x=1280-260,y=10)
+add_button=tk.Button(frame1,text="Add student",font=('Arial',15,"bold"),bg='#F0A04B',borderwidth=2, relief="solid")
+add_button.place(x=10,y=25,height=100,width=225)
+
+update_button=tk.Button(frame1,text="Update student",font=('Arial',15,"bold"),bg='#F39E60',borderwidth=2, relief="solid")
+update_button.place(x=10,y=150,height=100,width=225)
+Delete_button=tk.Button(frame1,text="Delete student",font=('Arial',15,"bold"),bg='#FFDAB3',borderwidth=2, relief="solid")  
+Delete_button.place(x=10,y=275,height=100,width=225)
+refresh_button=tk.Button(frame1,text="Refresh database",font=('Arial',15,"bold"),bg='#FADA7A',borderwidth=2, relief="solid")  
+refresh_button.place(x=10,y=400,height=100,width=225)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+#hello=("mohamed","amine","HELLO@gmail.com",15,8,2022)
 #print(search(connection,11))
-req=turn_to_json(*hello)
+#req=turn_to_json(*hello)
 #insert(connection,req)
 #print(delete(connection,4))
-print(update(connection,22,req))
+#print(update(connection,22,req))
 #print(show_all(connection))
 
-close_connection(connection)
+#close_connection(connection)
+root.mainloop()
